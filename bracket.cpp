ba5790bb1c1932f981138e22044f3fc5d49cddae
1kovand1 <alexkovbs@gmail.com>
2024-04-02 16:16:47 +0300

    task5

---
+++ bracket.cpp
@@ -0,0 +1,63 @@
+#include "bracket.h"
+#include <stack>
+
+bool isOpeningBracket(char c)
+{
+    switch (c)
+    {
+    case '(':
+    case '{':
+    case '[':
+    case '<':
+        return true;
+    }
+    return false;
+}
+
+bool isClosingBracket(char c)
+{
+    switch (c)
+    {
+    case ')':
+    case '}':
+    case ']':
+    case '>':
+        return true;
+    }
+    return false;
+}
+
+char getPair(char c)
+{
+    switch (c)
+    {
+    case ')':
+        return '(';
+    case ']':
+        return '[';
+    case '{':
+        return '}';
+    case '>':
+        return '<';
+    }
+
+    return 0;
+}
+
+bool checkBrackets(std::string str);
+{
+    std::stack<char> s;
+    for (c : str)
+    {
+        if (isOpeningBracket(c))
+            s.push(c);
+        else if (isClosingBracket(c))
+        {
+            if (s.empty() || s.top() != getPair(c))
+                return false;
+            s.pop();
+        }
+    }
+
+    return s.empty();
+}
