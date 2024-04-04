9d1b2c176532281f36ec3e304622622dc30fc0f8
1kovand1 <alexkovbs@gmail.com>
2024-04-04 18:28:19 +0300

    task4

---
+++ volume.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+
+int getMaxVolume(int a, int b, int c, int n, int* arr) {
+    int const* maxStart = values;
+    int max = 0;
+
+    for (int const* i = values; i + a < values + n; ++i)
+        if (std::min(*i, i[a]) > max && std::min(*i, i[a]) <= c)
+        {
+            max = std::min({ *i, i[a] });
+            maxStart = i;
+        }
+
+    auto i1 = maxStart, i2 = maxStart + std::min(a, n - 1);
+    max *= a;
+
+    while (i1 != i2)
+    {
+        if (std::min(*i1, *i2) <= c)
+            max = std::max<int>(max, (i2 - i1) * std::min(*i1, *i2));
+        if (*i1 == std::min(*i1, *i2))
+            i1++;
+        else
+            i2--;
+    }
+    return b * max;
+}
